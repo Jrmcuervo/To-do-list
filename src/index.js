dc91ba1b523aa965d2eaab6d6bdcb7203b6d5e64
Jrmcuervo <jrmcuervom@gmail.com>
2023-04-07 19:09:53 -0600

    Build functions for add and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Complete Microverse project',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Go to the gym',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 3,
-  },
-];
+let tasks = [];
 
 function renderTasks() {
   const list = document.querySelector('#todo-list');
+
+  // Clear the previous content of the list
+  list.innerHTML = '';
 
   tasks.forEach((task, index) => {
     const li = document.createElement('li');
@@ -31,9 +18,91 @@ function renderTasks() {
       li.classList.add('completed');
     }
     list.appendChild(li);
+
+    const checkboxInput = li.querySelector(`#task-${index}`);
+    checkboxInput.addEventListener('change', (event) => {
+      const checkbox = event.target;
+      const index = parseInt(checkbox.id.split('-')[1], 10);
+      tasks[index].completed = checkbox.checked;
+      renderTasks();
+    });
   });
+}
+
+function addTask(description) {
+  // Check if a task with the same description (ignoring case) already exists in the tasks array
+  const existingTask = tasks.find(
+    (task) => task.description.toLowerCase() === description.toLowerCase(),
+  );
+  if (existingTask) {
+    // Display an error message to the user
+    const error = document.createElement('p');
+    error.innerText = 'This task is already on the list';
+    error.classList.add('error');
+    const input = document.querySelector('#new-task-input');
+    input.parentNode.insertBefore(error, input.nextSibling);
+    // Remove the error message after 3 seconds
+    setTimeout(() => {
+      error.remove();
+    }, 3000);
+    return; // Don't add the new task if it already exists
+  }
+
+  const task = {
+    description,
+    completed: false,
+    index: tasks.length + 1,
+  };
+  tasks.push(task);
+  renderTasks();
+}
+
+function deleteTask(event, index) {
+  event.stopPropagation(); // Prevent the click event from propagating
+  tasks.splice(index, 1);
+  renderTasks();
+}
+
+function clearCompletedTasks() {
+  tasks = tasks.filter((task) => !task.completed);
+  renderTasks();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   renderTasks();
+
+  const list = document.querySelector('#todo-list');
+  const input = document.querySelector('#new-task-input');
+  const clearCompletedButton = document.querySelector('#clear-completed');
+
+  list.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target.tagName === 'SPAN' && target.innerText === 'more_vert') {
+      const li = target.parentNode;
+      const index = parseInt(li.querySelector('input').id.split('-')[1], 10);
+      const deleteButton = document.createElement('span');
+      deleteButton.innerHTML = 'delete';
+      deleteButton.classList.add('material-symbols-outlined', 'delete-button');
+      const icon = li.querySelector('span');
+      icon.replaceWith(deleteButton); // Replace the more_vert icon with the delete button
+      deleteButton.addEventListener('click', (event) => {
+        deleteTask(event, index);
+      });
+    }
+  });
+
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const description = input.value.trim();
+      if (description) {
+        addTask(description);
+        input.value = '';
+      }
+    }
+  });
+
+  clearCompletedButton.addEventListener('click', () => {
+    clearCompletedTasks();
+  });
 });
